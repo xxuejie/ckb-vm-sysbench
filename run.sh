1d80df56db9a2472224a7a05413b774a0f164e31
Xuejie Xiao <xxuejie@gmail.com>
2022-12-31 06:52:18 +0000

    Update

--- run.sh
+++ run.sh
@@ -7,7 +7,6 @@ sudo apt-get install -y build-essential linux-tools-common linux-tools-generic l
 
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 source ~/.cargo/env
-cargo install inferno
 
 git clone "$1" ckb-vm
 cd ckb-vm
@@ -16,4 +15,5 @@ cd ..
 cargo build --release
 
 mkdir rundata
-sudo perf stat ./target/release/runner --tx-file data/gw_state_validator_0x9ad651b1771ad5514e1fd5a4e439a7bcc6a6f5a81e2e6918e8da8709c407529f.json.gz --cell-index 0 --cell-type input --script-type type > rundata/gw.log
+./target/release/runner --tx-file data/gw_state_validator_0x9ad651b1771ad5514e1fd5a4e439a7bcc6a6f5a81e2e6918e8da8709c407529f.json.gz --cell-index 0 --cell-type input --script-type type > rundata/gw.log
+tar czf rundata.tar.gz rundata
